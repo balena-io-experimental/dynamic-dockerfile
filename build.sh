947d579190665e4de50a38c441c5868f3cb6efd5
Eugene Mirotin <emirotin@gmail.com>
2016-06-23 14:55:56 +0300

    delete unsupported and rename node version folders

--- build.sh
+++ build.sh
@@ -1,1 +1,27 @@
+#!/usr/bin/env bash
+
 ./node_modules/.bin/coffee ./build/build.coffee
+
+versionRe='[0-9]+\.[0-9]+'
+defaultVersion='0.10.22'
+
+for folder in $(ls -d dist/*/*)
+do
+  pushd $(pwd) > /dev/null
+  cd ./$folder
+  for version in $(ls -d *)
+  do
+    if [ ! -f $version/Dockerfile ]; then
+      rm -rf $folder/$version
+      continue
+    fi
+
+    if [ $version == $defaultVersion ]; then
+      baseVersion='default'
+    else
+      [[ $version =~ $versionRe ]] && baseVersion=${BASH_REMATCH[0]}
+    fi
+    mv ./$version ./$baseVersion
+  done
+  popd > /dev/null
+done
